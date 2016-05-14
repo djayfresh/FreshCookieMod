ac37c9efde4cd9f91f80bbfc783a1c61d754774f
djayfresh <djfresh1@comcast.net>
2016-05-13 22:03:11 -0500

    My First AI, with mob as Factory Worker

--- Source/src/minecraft/freshcaa/fresh/entity/FactoryWorker.java
+++ Source/src/minecraft/freshcaa/fresh/entity/FactoryWorker.java
@@ -1,35 +1,53 @@
 package freshcaa.fresh.entity;
 
+import freshcaa.fresh.cookies.CookieMod;
+import freshcaa.fresh.entity.ai.EntityAICircles;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.IMerchant;
+import net.minecraft.entity.INpc;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAIFollowGolem;
 import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookAtTradePlayer;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIMoveIndoors;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAIOpenDoor;
 import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAIPlay;
+import net.minecraft.entity.ai.EntityAIRestrictOpenDoor;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAITradePlayer;
+import net.minecraft.entity.ai.EntityAIVillagerMate;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.EntityAIWatchClosest2;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
 import net.minecraft.world.World;
 
-public class FactoryWorker extends EntityAnimal
+public class FactoryWorker extends EntityAgeable implements IMerchant, INpc
 {
+	public static String NAME = "factoryworker";
+	protected int randomTickDivider;
+	
     public FactoryWorker(World par1World)
     {
         super(par1World);
         this.setSize(0.9F, 1.3F);
         this.getNavigator().setAvoidsWater(true);
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIPanic(this, 2.0D));
-        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
-        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, Item.wheat.itemID, false));
-        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
-        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-        this.tasks.addTask(7, new EntityAILookIdle(this));
+        this.getNavigator().setBreakDoors(true);
+        this.tasks.addTask(0, new EntityAICircles(this, SharedMonsterAttributes.movementSpeed.getDefaultValue(), 20.0f));
     }
 
     @Override
@@ -43,19 +61,19 @@ public class FactoryWorker extends EntityAnimal
     @Override
     protected String getLivingSound()
     {
-        return "yourmod:YourSound";//this refers to:yourmod/sound/YourSound
+        return CookieMod.modid + ":" + NAME + ".living";//this refers to:yourmod/sound/YourSound
     }
 
     @Override
     protected String getHurtSound()
     {
-        return "yourmod:optionalFile.YourSound";//this refers to:yourmod/sound/optionalFile/YourSound
+        return CookieMod.modid + ":" + NAME + ".hurt";//this refers to:yourmod/sound/optionalFile/YourSound
     }
 
     @Override
     protected String getDeathSound()
     {
-        return "yourmod:optionalFile.optionalFile2.YourSound";//etc.
+        return CookieMod.modid + ":" + NAME + ".death";//etc.
     }
 
     @Override
@@ -69,4 +87,67 @@ public class FactoryWorker extends EntityAnimal
 	{
 		return null;
 	}
+
+	@Override
+	public void setCustomer(EntityPlayer entityplayer)
+	{
+		
+	}
+
+	@Override
+	public EntityPlayer getCustomer()
+	{
+		return null;
+	}
+
+	@Override
+	public MerchantRecipeList getRecipes(EntityPlayer entityplayer)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRecipes(MerchantRecipeList merchantrecipelist)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void useRecipe(MerchantRecipe merchantrecipe)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void func_110297_a_(ItemStack itemstack)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+     * Returns true if the newer Entity AI code should be run
+     */
+    protected boolean isAIEnabled()
+    {
+        return true;
+    }
+
+    /**
+     * Determines if an entity can be despawned, used on idle far away entities
+     */
+    protected boolean canDespawn()
+    {
+        return false;
+    }
+    protected void updateAITick()
+    {
+        if (--this.randomTickDivider <= 0)
+        {
+        	randomTickDivider = 80 + getRNG().nextInt(50);
+        }
+    }
 }
---
+++ Source/src/minecraft/freshcaa/fresh/entity/ai/EntityAICircles.java
@@ -0,0 +1,62 @@
+package freshcaa.fresh.entity.ai;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.util.Vec3;
+
+public class EntityAICircles extends EntityAIBase
+{
+	private EntityLiving entity;
+	private double maxWalkDistance;
+	private Vec3 target;
+	private double speed;
+	
+	public EntityAICircles(EntityLiving entity, double speed, double maxWalkDistance)
+	{
+		this.entity = entity;
+		this.speed = speed;
+		this.maxWalkDistance = maxWalkDistance;
+	}
+	
+	@Override
+	public boolean shouldExecute()
+	{
+		if(target == null)
+		{
+			float randX = this.entity.worldObj.rand.nextFloat();
+			float randZ = this.entity.worldObj.rand.nextFloat();
+	    	double posX = this.entity.posX + (randX * maxWalkDistance) - (maxWalkDistance * 0.5f);
+	    	double posZ = this.entity.posZ + (randZ * maxWalkDistance) - (maxWalkDistance * 0.5f);
+	    	target = this.entity.worldObj.getWorldVec3Pool().getVecFromPool(posX, this.entity.posY, posZ);
+	    	System.out.println("Set Target: " + String.format("%1$.3f, %2$.3f, %3$.3f", target.xCoord, target.yCoord, target.zCoord));
+	    	this.entity.getLookHelper().setLookPosition(posX, this.entity.posY + 2, posZ, 30.0F, 30.0F);
+		}
+		System.out.println("should execute");
+		return this.entity.worldObj.isDaytime() && target != null;
+	}
+
+	@Override
+	public boolean continueExecuting()
+    {
+        return !this.entity.getNavigator().noPath();
+    }
+	
+    public void resetTask()
+    {
+    	System.out.println("Task Reset");
+        this.target = null;
+    }
+	
+	@Override
+    public void startExecuting() 
+	{
+    	boolean resetPath = this.entity.getNavigator().tryMoveToXYZ(target.xCoord, target.yCoord, target.zCoord, speed);
+    	
+    	if(!this.entity.getNavigator().noPath() || resetPath)
+    	{
+    		System.out.println("Path reset");
+    		target = null;
+    	}
+    }
+}
